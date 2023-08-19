f46df0e56dca1e66b664a6c875b95cc2239a954b
Carol Lima <ac.limarin@gmail.com>
2023-08-18 21:01:58 -0300

    Init

---
+++ client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hi! I'm the client! Please insert the server's access data:");
+        Console.WriteLine("IP Address:");
+        var serverIp = Console.ReadLine();
+
+        Console.WriteLine("Port:");
+        var port = Convert.ToInt32(Console.ReadLine());
+
+        // Create TCP/IP socket
+        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
+        // Connect
+        clientSocket.Connect(serverIp, port);
+        
+        Console.WriteLine("OK, i'm in. Now write a message to the server");
+        var message = Console.ReadLine();
+        
+        var data = Encoding.UTF8.GetBytes(message);
+        clientSocket.Send(data);
+
+        var responseData = new byte[1024];
+        var bytesRead = clientSocket.Receive(responseData);
+        var response = Encoding.UTF8.GetString(responseData, 0, bytesRead);
+        Console.WriteLine($"Server response: { response }");
+
+        clientSocket.Close();
+        
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var ipAdress = IPAddress.Any;
+        var port = 1234;
+
+        //Create TCP/IP socket
+        var serverSocket = new Socket(ipAdress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        //Bind and listen
+        serverSocket.Bind(new IPEndPoint(ipAdress, port));
+        serverSocket.Listen(10);
+
+
+        var localIp = Dns.GetHostAddresses(Dns.GetHostName())
+            .Where(x => x.AddressFamily == AddressFamily.InterNetwork)
+            .FirstOrDefault();
+
+        Console.WriteLine($"Hi! I'm the Server and this is my access data:");
+        Console.WriteLine($"IP Address: { localIp }");
+        Console.WriteLine($"Port: { port }");
+        
+        
+        Console.WriteLine("");
+        Console.WriteLine("Now i'm waithing for a client...");
+
+        //Accept connection from client
+        var clientSocket = serverSocket.Accept();
+
+        Console.WriteLine("Client connected!");
+
+        //Receive data from client
+        var data = new byte[1024];
+        var byteRead = clientSocket.Receive(data);
+        var message = Encoding.UTF8.GetString(data, 0, byteRead);
+        Console.WriteLine($"Received: { message }");
+
+        //Respond back
+        var response = "Message received successfully!";
+        var responseData = Encoding.UTF8.GetBytes(response);
+        clientSocket.Send(responseData);
+
+        clientSocket.Close();
+        serverSocket.Close();
+    }
+}
